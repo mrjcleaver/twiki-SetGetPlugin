e5be73cd786f6996a0149f2f9335123c3909dba3
Martin Cleaver <Martin@Cleaver.org>
2012-04-09 13:31:44 -0400

    Scoping namespace tests, and possible use of Storable instead of eval

--- test/unit/SetGetPlugin/SetGetPluginTests.pm
+++ test/unit/SetGetPlugin/SetGetPluginTests.pm
@@ -15,11 +15,14 @@ use Error qw( :try );
 # cd installdir/core/test/unit/
 # perl ../bin/TestRunner.pl -clean SetGetPlugin/SetGetPluginSuite.pm > SetGetPlugin/test-run-output/1.out 2>&1
 # Use graphical debugger ptkdb and a breakpoint inserted into code
-# using $DB::single = 1; 
+# using $DB::single = 1;
+# and perl -d:ptkdb ../bin/TestRunner.pl ...
 # http://stackoverflow.com/questions/4691448/can-i-insert-break-point-into-source-perl-program
 
 
 my $debug = 1;
+my $tempStoreFile_not_namespaced = '/tmp/storeFile_not_namespaced';
+my $tempStoreFile_namespaced = '/tmp/storeFile_namespaced';
 
 # Set up the test fixture
 sub set_up {
@@ -36,12 +39,53 @@ sub set_up {
     $this->{twiki}->{store}->createWeb(
         $this->{twiki}->{user}, $this->{target_web2}
     );
+    set_up_save_persistent_vars_not_namespaced();
+    set_up_save_persistent_vars_namespaced();
+}
+
+
+sub set_up_save_persistent_vars_not_namespaced {
+    my $PersistentVars = {
+                        'HoldMilestoneID00153_UNIX' => '1333131708',
+                        'HoldMilestoneID00293_TIME' => '2535',
+                        'MilestoneID00097_BL' => '1333132500',
+                        'HoldMilestoneID00169_UNIX' => '1333149708',
+                        'MilestoneID00078_UNIX' => '1333116558',
+                        'HoldMilestoneID00349_BL' => '1333128300',
+    };
+    # copied from SetGetPlugin::Core    
+    my $text = Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
+    TWiki::Func::saveFile( $tempStoreFile_not_namespaced, $text ) ;
+}
+
+
+sub set_up_save_persistent_vars_namespaced {
+    # Some namespaced, others not
+    my $PersistentVars = { 
+                            'ns1' => {
+                                'HoldMilestoneID00153_UNIX' => '1333131708',
+                                'HoldMilestoneID00293_TIME' => '2535',
+                            },
+                            'ns2' => {
+                                'MilestoneID00097_BL' => '1333132500',
+                                'HoldMilestoneID00169_UNIX' => '1333149708',
+                            },
+                            'MilestoneID00078_UNIX' => '1333116558',
+                            'HoldMilestoneID00349_BL' => '1333128300',                            
+    };
+    # copied from SetGetPlugin::Core    
+    my $text = Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
+    TWiki::Func::saveFile( $tempStoreFile_namespaced, $text ) ;
 }
 
 sub tear_down {
     my $this = shift;
     $this->{twiki}->{store}->removeWeb($this->{twiki}->{user}, $this->{target_web});
     $this->{twiki}->{store}->removeWeb($this->{twiki}->{user}, $this->{target_web2});
+    
+    unlink $tempStoreFile_namespaced;
+    unlink $tempStoreFile_not_namespaced;
+    
     $this->SUPER::tear_down();
 }
 
@@ -131,7 +175,6 @@ sub test_set_doesnt_care_about_web {
     
     $this->assert_equals('v2', $actual1);
     $this->assert_equals('v2', $actual2);
-
 }
 
 sub test_set_namespace_volatile_should_be_ignored {
@@ -149,20 +192,28 @@ sub test_set_namespace_volatile_should_be_ignored {
     $this->assert_equals('v2', $actual);
 }
 
+=pod
+Assumption - users can create as many namespaces as they like, the administrator does not need to configure them
+
+=cut
+
 sub test_set_namespace_remember_needs_to_be_written {
     my $this = shift;
     
     my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($core, {_DEFAULT => "remembered", value => "v0", remember=>"1"});    # not namespaced
     $this->do_set($core, {_DEFAULT => "remembered", value => "v1", namespace => "ns1", remember=>"1"});
     $this->do_set($core, {_DEFAULT => "remembered", value => "v2", namespace => "ns2", remember=>"1"});
     
     my $actual = $this->do_get($core, {_DEFAULT => "volatile", namespace=> "ns1"});
     my $actual2 = $this->do_get($core, {_DEFAULT => "volatile", namespace=> "ns2"});
+    my $actual_unnamespaced = $this->do_get($core, {_DEFAULT => "volatile"});
 
-    $this->assert('key: remembered, value: v1 <br />\nkey: remembered, value: v2 <br />', $this->do_dump($core)); 
+    $this->assert('key: remembered, value: v1 <br />\nkey: remembered, value: v2 <br />',
+                  $this->do_dump($core)); # TODO fix up with namespaces
+    $this->assert_equals('v0', $actual_unnamespaced);
     $this->assert_equals('v1', $actual);
-    $this->assert_equals('v2', $actual);
-
+    $this->assert_equals('v2', $actual2);
 }
 
 sub test_set_scope_not_remembered_does_not_need_to_be_written {
@@ -179,8 +230,15 @@ sub test_set_scope_not_remembered_does_not_need_to_be_written {
     $this->assert_equals('v2', $actual);
 }
 
+=pod
+Tests with a known good scope and remember=1
+
+=cut
+
 sub test_set_scope_remember_needs_to_be_written {
     my $this = shift;
+    
+    $TWiki::cfg{SetGetPlugin}{Scopes} = 'sc1, sc2';    
     
     my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
     $this->do_set($core, {_DEFAULT => "remembered_scoped", value => "v1", scope => "sc1", remember => "1"});
@@ -193,5 +251,81 @@ sub test_set_scope_remember_needs_to_be_written {
     $this->assert_equals('v1', $actual);
     $this->assert_equals('v2', $actual);
 }
+
+=pod
+Tests with a known bad scope and remember=1
+=cut
+
+sub test_set_scope_remember_bad_scope_needs_to_be_written {
+    my $this = shift;
+    
+    $TWiki::cfg{SetGetPlugin}{Scopes} = 'ScopeA, ScopeB';
+    
+    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($core, {_DEFAULT => "remembered_scoped", value => "v1", scope => "nonExistantScope", remember => "1"});
+    
+    my $actual = $this->do_get($core, {_DEFAULT => "remembered_scoped", scope=> "sc1"});
+    # TODO - do we want to log bad accesses?
+
+    $this->assert('', $this->do_dump($core)); 
+    $this->assert_equals('', $actual); # Scope "nonExistantScope" doesn't exist - so no value should be written
+}
+
+sub load_persistent_vars {
+    my ($tempStoreFile) = @_;
+    # copied from SetGetPlugin::Core
+        my $text = TWiki::Func::readFile( $tempStoreFile );
+        $text =~ /^(.*)$/gs; # untaint, it's safe
+        $text = $1;
+
+    $DB::single = 1;
+    
+    our $PersistentVars;
+    $PersistentVars = eval($text);
+    return $PersistentVars;
+}
+
+
+sub test_load_persistent_vars {
+    my $this = shift;
+    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+
+    my $PersistentVars = load_persistent_vars($tempStoreFile_not_namespaced);
+    print Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
+
+    #my $vars = load_into_storable($tempStoreFile_not_namespaced);
+    
+    $this->do_dump($core); # differnt 
+    
+    $PersistentVars = load_persistent_vars($tempStoreFile_namespaced);
+    print Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
+
+    $DB::single = 1;
+
+    #my $vars2 = load_into_storable($tempStoreFile_namespaced);
+    
+    $this->do_dump($core);
+}
+
+####### If we need to move to Storable instead of eval. 
+
+use Storable qw(lock_store lock_nstore lock_retrieve);
+
+sub load_into_storable {
+    my ($tempStoreFile) = @_;
+    
+    my $hashref = lock_retrieve($tempStoreFile);    
+    return $hashref;
+}
+
+
+sub convert_persistent_vars_to_storeable {
+
+    my ($persistentVarsStoreFile) = @_;
+    my $storableStoreFile = $persistentVarsStoreFile.".storable";
+    my $vars = load_persistent_vars($persistentVarsStoreFile);
+    lock_store($vars, $storableStoreFile);
+}
+
 
 1;
