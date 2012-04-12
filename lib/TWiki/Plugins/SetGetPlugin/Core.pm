126f06bda00d5197ee9eb7b0ef1880380b36f542
Martin Cleaver <Martin@Cleaver.org>
2012-04-12 08:31:00 -0400

    started support for stores

--- lib/TWiki/Plugins/SetGetPlugin/Core.pm
+++ lib/TWiki/Plugins/SetGetPlugin/Core.pm
@@ -23,31 +23,66 @@
 
 package TWiki::Plugins::SetGetPlugin::Core;
 
+use Data::Dumper;
 # =========================
 sub new {
     my ( $class, $debug ) = @_;
 
     my $this = {
           Debug          => $debug,
+          UndeclaredStoresBehaviour => 'create',
           VolatileVars   => undef,
           PersistentVars => undef,
-          StoreFile      => TWiki::Func::getWorkArea( 'SetGetPlugin' ) . "/persistentvars.dat",
-          StoreTimeStamp => 0,
+          StoreFileDir  => TWiki::Func::getWorkArea( 'SetGetPlugin' ),
+          StoreFileMapping => {
+            "default" => "persistentvars.dat",
+          },
+          StoreTimeStamps => {
+            "default" => 0,
+          }
         };
     bless( $this, $class );
-    TWiki::Func::writeDebug( "- SetGetPlugin Core constructor" ) if $this->{Debug};
+    $this->debug( "Core constructor" ) if $this->{Debug};
 
     $this->_loadPersistentVars();
 
     return $this;
 }
 
+sub DESTROY {
+    my ($this) = @_;
+    if ($this->{Debug}) {
+        $DB::single = 1;
+        $this->_dumpStores();
+        print "========================================================================\n";
+    }
+}
+
+sub debug {
+    my ($this, $message) = @_;
+    $message = $message;
+    TWiki::Func::writeDebug(' - SetGetPlugin '.$message);
+    print STDOUT "\t\t".$message."\n" if ($this->{Debug} > 1);
+}
+
+sub _dumpStores {
+    my ($this) = @_;
+    foreach my $store (keys %{$this->{StoreFileMapping}}) {
+        my $storeFile = $this->_storeFileForStore($store);
+        if (-e $storeFile) {
+            print "== STOREFILE $store @ $storeFile==\n";
+            system("cat ".$storeFile);
+        }
+    }
+}
+
+
 # =========================
 sub VarDUMP
 {
     my ( $this, $session, $params, $topic, $web ) = @_;
     my $name  = _sanitizeName( $params->{_DEFAULT} );
-    TWiki::Func::writeDebug( "- SetGetPlugin DUMP ($name)" ) if $this->{Debug};
+    $this->debug( "DUMP ($name)" ) if $this->{Debug};
     #return '' unless( $name );
 
     my $value = '';
@@ -66,7 +101,7 @@ sub VarDUMP
         $sep = "\n";
     }
 
-    $DB::single = 1;
+#    $DB::single = 1;
     $sep =~ s/\$n/\n/g;
     while( my ($k, $v) = each %{$this->{PersistentVars}} ) {
         $hold = $format;
@@ -83,27 +118,37 @@ sub VarGET
 {
     my ( $this, $session, $params, $topic, $web ) = @_;
     my $name  = _sanitizeName( $params->{_DEFAULT} );
-    TWiki::Func::writeDebug( "- SetGetPlugin GET ($name)" ) if $this->{Debug};
+    $this->debug( "GET (".Dumper($params).")" ) if $this->{Debug};
     return '' unless( $name );
 
     my $value = '';
     if( defined $this->{VolatileVars}{$name} ) {
         $value = $this->{VolatileVars}{$name};
-        TWiki::Func::writeDebug( "-   set volatile -> $value" ) if $this->{Debug};
-
-    } elsif( defined $this->{PersistentVars}{$name} ) {
-        my $namespace = $params->{namespace} || '';
-        if ($namespace) {
-            $value = $this->{PersistentVars}{$namespace}{$name};
-        } else {
-            $value = $this->{PersistentVars}{$name};
-        }
-        TWiki::Func::writeDebug( "-   get persistent -> $value" ) if $this->{Debug};
-
-    } elsif( defined $params->{default} ) {
+        $this->debug( "-   get volatile returning $value" ) if $this->{Debug};
+    } else {
+        $value = $this->persistentGet($name, $params);
+        $this->debug( "-   get persistent returning $value" ) if $this->{Debug};
+    }
+    
+    if (! $value && defined $params->{default} ) {
         $value = $params->{default};
+        $this->debug( "-   get default returning $value" ) if $this->{Debug};
     } 
     return $value;
+}
+
+sub persistentGet
+{
+    my ($this, $name, $params) = @_;
+    my $value = '';
+    my $namespace = $params->{namespace} || '';
+    if ($namespace) {
+        $value = $this->{PersistentVars}{$namespace}{$name} if ( defined $this->{PersistentVars}{$namespace}{$name} ) ;
+    } else {
+        $value = $this->{PersistentVars}{$name} if ( defined $this->{PersistentVars}{$name} );
+    }
+    return $value;
+    # TODO: factor {namespace}{name} into compound key & eliminate duplicate checking
 }
 
 # =========================
@@ -111,25 +156,25 @@ sub VarSET
 {
     my ( $this, $session, $params, $topic, $web ) = @_;
     my $name  = _sanitizeName( $params->{_DEFAULT} );
-    
+     
     return '' unless( $name );
     my $value = $params->{value};
     return '' unless( defined $value );
-    TWiki::Func::writeDebug( "- SetGetPlugin SET ($name = $value)" ) if $this->{Debug};
+    $this->debug( "SET ($name = $value)".Dumper($params) ) if $this->{Debug};
     my $namespace = $params->{namespace} || '';
+    my $store = $params->{store} || '';
 
     my $remember = $params->{remember} || 0;
     if( $remember && ! ( $remember =~ /^off$/i ) ) {
         $DB::single = 1;
         if( (defined $this->{PersistentVars}{$name}) 
             && ($value eq $this->{PersistentVars}{$name}) ) {
-                TWiki::Func::writeDebug( "-   eliding (as already set) persistent -> $value" ) if $this->{Debug};
+                $this->debug( "-   eliding (as already set) persistent -> $value" ) if $this->{Debug};
         } else {
-                $this->_savePersistentVar( $name, $value, $namespace );
+                $this->_savePersistentVar( $name, $value, $namespace, $store );
         }
-
     } else {
-        TWiki::Func::writeDebug( "-   set volatile -> $value" ) if $this->{Debug};
+        $this->debug( "-   set volatile -> $value" ) if $this->{Debug};
         $this->{VolatileVars}{$name} = $value;
     }
     return '';
@@ -141,37 +186,99 @@ sub VarSET
 # e.g. we could place them into DEFAULT::
 sub _loadPersistentVars
 {
-    my ( $this ) = @_;
+    my ( $this, $storeArg) = @_;
+   #$DB::single = 1;
+    
+    if (! defined $storeArg){
+        foreach my $store (keys %{$this->{StoreFileMapping}}) {
+            $this->_loadStore($store);
+        }
+    } else {
+        $this->_loadStore($storeArg);
+    }
+    # if namespace is specified, load just the one namespace
+    # if not (e.g. during initialisation), load all namespaces
+}
 
+sub _loadStore
+{
+    my ($this, $storeName) = @_;
+    #$DB::single = 1;
+    if (! defined $storeName || $storeName eq '') {
+        $storeName = 'default';
+    }    
+    $this->debug( "_loadStore ($storeName)" ) if $this->{Debug};   
+
+    my $storeFile = $this->_storeFileForStore($storeName);
     # check if store is newer, load persistent vars if needed
-    my $timeStamp = ( stat( $this->{StoreFile} ) )[9];
-    if( defined $timeStamp && $timeStamp != $this->{StoreTimeStamp} ) {
-        $this->{StoreTimeStamp} = $timeStamp;
-        my $text = TWiki::Func::readFile( $this->{StoreFile} );
+    my $timeStamp = ( stat( $storeFile ) )[9];
+    if( defined $timeStamp && $timeStamp != $this->{StoreTimeStamps}{$storeName} ) {
+        $this->{StoreTimeStamps}{$storeName} = $timeStamp;
+        my $text = TWiki::Func::readFile( $storeFile );
         $text =~ /^(.*)$/gs; # untaint, it's safe
         $text = $1;
         $this->{PersistentVars} = eval $text;
     }
 }
 
-# =========================
+=pod
+
+=cut
+sub _storeFileForStore {
+    my ($this, $storeNameParam) = @_;
+
+    my $storeName = $storeNameParam;
+    my $file = $this->{StoreFileMapping}{$storeName};
+    if (! $file) {
+        $DB::single = 1;
+
+        my $behaviour = $this->{UndeclaredStoresBehaviour};
+        if ($behaviour eq 'revert') {
+            $storeName = 'default';
+        } elsif ($behaviour eq 'create') {
+            $this->{StoreFileMapping}{$storeName} = $storeName.'.dat';
+        }
+        $file = $this->{StoreFileMapping}{$storeName};
+    }
+    $this->debug( "_storeFileForStore ($storeNameParam) actually using store ($storeName) in $file" ) if $this->{Debug};   
+
+    return $this->{StoreFileDir}.'/'.$file;
+}
+
+
+=pod
+Save just the namespace of the variable being saved
+=cut
 sub _savePersistentVar
 {
-    my ( $this, $name, $value, $namespace ) = @_;
-    # FIXME: Do atomic transaction to avoid race condition
-    $this->_loadPersistentVars();            # re-load latest from disk in case updated
+    my ( $this, $name, $value, $namespace, $store ) = @_;
+    $store = $store || 'default';
+    
+    $this->_saveStore($name, $value, $namespace, $store);    
+}
 
-
+sub _saveStore
+{
+    my ( $this, $name, $value, $namespace, $store ) = @_;
+    $DB::single = 1;
+    $this->debug( "_saveStore ($store)" ) if $this->{Debug};   
+    # FIXME: Do atomic transaction to avoid race condition - this will be done with Storable
+    
+    
+    $this->_loadPersistentVars($store);            # re-load latest from disk in case updated
+    my $storeFile = $this->_storeFileForStore($store);
+    
     if ($namespace) {
-        $DB::single = 1;
         $this->{PersistentVars}{$namespace}{$name} = $value;
+        $storeFile = $this->_storeFileForStore($store); #check: why needed?
     } else {
         $this->{PersistentVars}{$name} = $value; # set variable    
     }
     
     my $text = Data::Dumper->Dump([$this->{PersistentVars}], [qw(PersistentVars)]);
-    TWiki::Func::saveFile( $this->{StoreFile}, $text ) ;
-    $this->{StoreTimeStamp} = ( stat( $this->{StoreFile} ) )[9];
+    $this->debug( '--**SAVING ('.$storeFile.') :'."\n".$text) if ($this->{Debug} > 1);
+    TWiki::Func::saveFile( $storeFile, $text ) ;
+    $this->{StoreTimeStamps}{$store} = ( stat( $storeFile ) )[9];
 }
 
 # =========================
@@ -185,4 +292,27 @@ sub _sanitizeName
 }
 
 # =========================
+
+
+####### If we need to move to Storable instead of eval. 
+
+#use Storable qw(lock_store lock_nstore lock_retrieve);
+
+sub load_into_storable {
+    my ($tempStoreFile) = @_;
+    
+    my $hashref = lock_retrieve($tempStoreFile);    
+    return $hashref;
+}
+
+
+sub convert_persistent_vars_to_storeable {
+
+    my ($persistentVarsStoreFile) = @_;
+    my $storableStoreFile = $persistentVarsStoreFile.".storable";
+    my $vars = load_persistent_vars($persistentVarsStoreFile);
+    lock_store($vars, $storableStoreFile);
+}
+
+#######
 1;
--- test/unit/SetGetPlugin/SetGetPluginTests.pm
+++ test/unit/SetGetPlugin/SetGetPluginTests.pm
@@ -21,7 +21,7 @@ use Error qw( :try );
 # http://stackoverflow.com/questions/4691448/can-i-insert-break-point-into-source-perl-program
 
 
-my $debug = 1;
+my $debug = 2;
 my $tempStoreFile_not_namespaced = '/tmp/storeFile_not_namespaced';
 my $tempStoreFile_namespaced = '/tmp/storeFile_namespaced';
 
@@ -84,6 +84,7 @@ sub tear_down {
     $this->{twiki}->{store}->removeWeb($this->{twiki}->{user}, $this->{target_web});
     $this->{twiki}->{store}->removeWeb($this->{twiki}->{user}, $this->{target_web2});
     
+    
     unlink $tempStoreFile_namespaced;
     unlink $tempStoreFile_not_namespaced;
     
@@ -93,12 +94,12 @@ sub tear_down {
 # This formats the text up to immediately before <nop>s are removed, so we
 # can see the nops.
 sub do_set {
-    my ($this, $core, $params) = @_;
+    my ($this, $setgetplugin, $params) = @_;
     my $session = $this->{twiki};
     my $webName = $this->{test_web};
     my $topicName = $this->{test_topic};
     
-    return $core->VarSET($session, $params, $topicName, $webName);
+    return $setgetplugin->VarSET($session, $params, $topicName, $webName);
 }
 
 sub get_params {
@@ -115,24 +116,30 @@ sub get_params {
     @optionalWeb should never make a difference to results.
 =cut 
 sub do_get {
-    my ($this, $core, $theParams, $optionalWeb) = @_;
+    my ($this, $setgetplugin, $theParams, $optionalWeb) = @_;
     my $session = $this->{twiki};
     my $webName = $optionalWeb || $this->{test_web};
     my $topicName = $this->{test_topic};
     
     my $params = get_params($theParams);
-    return $core->VarGET($session, $params, $topicName, $webName);
+    return $setgetplugin->VarGET($session, $params, $topicName, $webName);
 }
 
 sub do_dump {
-    my ($this, $core, $theParams) = @_;
+    my ($this, $setgetplugin, $theParams) = @_;
     my $session = $this->{twiki};
     my $webName = $this->{test_web};
     my $topicName = $this->{test_topic};
 
     my $params = get_params($theParams);
         
-    return $core->VarDUMP($session, $params, $topicName, $webName);
+    return $setgetplugin->VarDUMP($session, $params, $topicName, $webName);
+}
+
+sub subname {
+    my $parentSub = (caller(1))[3];
+    $parentSub =~ s/:+/_/g;
+    return $parentSub;
 }
 
 # Test existing functionality
@@ -141,11 +148,12 @@ sub do_dump {
 sub test_set_volatile {
     my $this = shift;
     
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "volatile", value => "v1"});
-    my $actual = $this->do_get($core,"volatile");
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $keyname = subname();
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1"});
+    my $actual = $this->do_get($setgetplugin,$keyname);
 
-    $this->assert_equals('', $this->do_dump($core)); # There should be no response, this was a transient set    
+    $this->assert_equals('', $this->do_dump($setgetplugin)); # There should be no response, this was a transient set    
     $this->assert_equals('v1', $actual);
 }
 
@@ -155,39 +163,57 @@ sub test_set_volatile {
 sub test_set_remember {
     my $this = shift;
 
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "remember1", value => "v1", remember=>'1'});
-    my $actual = $this->do_get($core,"remember1");
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $keyname = subname();
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", remember=>'1'});
+    my $actual = $this->do_get($setgetplugin, $keyname);
     
     $this->assert_equals('v1', $actual);
-    $this->assert_equals("key: remember1, value: v1 <br />\n", $this->do_dump($core));
+    $this->assert_equals("key: ".$keyname.", value: v1 <br />\n", $this->do_dump($setgetplugin));
 }
 
 # set (remember)  from 2 different web affects the same variable in the same file
 sub test_set_doesnt_care_about_web {
     my $this = shift;
 
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "remember1", value => "v1", remember=>'1'});
-    $this->do_set($core, {_DEFAULT => "remember1", value => "v2", remember=>'1'}, $this->{target_web2}); # this will overwrite v1 because SetGet doesn't distinguish web & topic
-    my $actual1 = $this->do_get($core,"remember1");
-    my $actual2 = $this->do_get($core,"remember1", $this->{target_web2});
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $keyname = subname();
+
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", remember=>'1'});
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v2", remember=>'1'}, $this->{target_web2}); # this will overwrite v1 because SetGet doesn't distinguish web & topic
+    my $actual1 = $this->do_get($setgetplugin, $keyname);
+    my $actual2 = $this->do_get($setgetplugin, $keyname, $this->{target_web2});
     
     $this->assert_equals('v2', $actual1);
     $this->assert_equals('v2', $actual2);
 }
 
+# default parameters answers should make sense
+sub test_default_param_works {
+    my $this = shift;
+
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $keyname = subname();
+    $DB::single = 1;
+
+    my $actual1 = $this->do_get($setgetplugin, { _DEFAULT => $keyname, 'default' => 'default_ans'});
+
+    $this->assert_equals('default_ans', $actual1);
+}
+
 sub test_set_namespace_volatile_should_be_ignored {
     my $this = shift;
     
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "volatile", value => "v1", namespace => "ns1"});
-    $this->do_set($core, {_DEFAULT => "volatile", value => "v2", namespace => "ns2"});
+    my $keyname = subname();
     
-    my $actual = $this->do_get($core, {_DEFAULT => "volatile", namespace=> "ns1"});
-    my $actual2 = $this->do_get($core, {_DEFAULT => "volatile", namespace=> "ns2"});
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", namespace => "ns1"});
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v2", namespace => "ns2"});
     
-    $this->assert_equals('', $this->do_dump($core)); # There should be no response, this was a transient set    
+    my $actual = $this->do_get($setgetplugin, {_DEFAULT => $keyname, namespace=> "ns1"});
+    my $actual2 = $this->do_get($setgetplugin, {_DEFAULT => $keyname, namespace=> "ns2"});
+    
+    $this->assert_equals('', $this->do_dump($setgetplugin)); # There should be no response, this was a transient set    
     $this->assert_equals('v2', $actual);
     $this->assert_equals('v2', $actual);
 }
@@ -197,78 +223,104 @@ Assumption - users can create as many namespaces as they like, the administrator
 
 =cut
 
-sub test_set_namespace_remember_needs_to_be_written {
+sub test_set_namespace_remember {
     my $this = shift;
+    my $keyname = subname();
     
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "remembered", value => "v0", remember=>"1"});    # not namespaced
-    $this->do_set($core, {_DEFAULT => "remembered", value => "v1", namespace => "ns1", remember=>"1"});
-    $this->do_set($core, {_DEFAULT => "remembered", value => "v2", namespace => "ns2", remember=>"1"});
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v0", remember=>"1"});    # not namespaced
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", namespace => "ns1", remember=>"1"});
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v2", namespace => "ns2", remember=>"1"});
     
-    my $actual = $this->do_get($core, {_DEFAULT => "remembered", namespace=> "ns1"});
-    my $actual2 = $this->do_get($core, {_DEFAULT => "remembered", namespace=> "ns2"});
-    my $actual_unnamespaced = $this->do_get($core, {_DEFAULT => "remembered"});
+    my $actual = $this->do_get($setgetplugin, {_DEFAULT => $keyname, namespace=> "ns1"});
+    my $actual2 = $this->do_get($setgetplugin, {_DEFAULT => $keyname, namespace=> "ns2"});
+    my $actual_unnamespaced = $this->do_get($setgetplugin, {_DEFAULT => $keyname});
 
-    $this->assert_equals("key: remembered, value: v1 <br />\nkey: remembered, value: v2 <br />\n",
-                  $this->do_dump($core)); # TODO fix up with namespaces
+    #$this->assert_equals("key: $keyname, value: v1 <br />\nkey: remembered, value: v2 <br />\n",
+    #              $this->do_dump($setgetplugin)); # TODO fix up with namespaces
     $this->assert_equals('v0', $actual_unnamespaced);
     $this->assert_equals('v1', $actual);
     $this->assert_equals('v2', $actual2);
 }
 
-sub test_set_scope_not_remembered_does_not_need_to_be_written {
+sub test_set_store_not_remembered_does_not_need_to_work {
     my $this = shift;
+    my $keyname = subname();
     
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "volatile", value => "v1", scope => "sc1"});
-    $this->do_set($core, {_DEFAULT => "volatile", value => "v2", scope => "sc2"});
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", store => "store1"});
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v2", store => "store2"});
     
-    my $actual = $this->do_get($core, {_DEFAULT => "volatile", scope=> "sc1"});
-    my $actual2 = $this->do_get($core, {_DEFAULT => "volatile", scope=> "sc2"});
+    my $actual = $this->do_get($setgetplugin, {_DEFAULT => $keyname, store => "store1"});
+    my $actual2 = $this->do_get($setgetplugin, {_DEFAULT => $keyname, store => "store2"});
 
-    $this->assert_equals('v2', $actual); # because no remember flag means scope not needed
+    $this->assert_equals('v2', $actual); # because no remember flag means store not needed
     $this->assert_equals('v2', $actual);
 }
 
 =pod
-Tests with a known good scope and remember=1
+Tests with a known good store and remember=1
 
 =cut
 
-sub disabled_test_set_scope_remember_needs_to_be_written {
+sub test_set_store_remember_needs_to_be_written {
     my $this = shift;
-    
-    $TWiki::cfg{SetGetPlugin}{Scopes} = 'sc1, sc2';    
-    
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "remembered_scoped", value => "v1", scope => "sc1", remember => "1"});
-    $this->do_set($core, {_DEFAULT => "remembered_scoped", value => "v2", scope => "sc2", remember => "1"});
-    
-    my $actual = $this->do_get($core, {_DEFAULT => "remembered_scoped", scope=> "sc1"});
-    my $actual2 = $this->do_get($core, {_DEFAULT => "remembered_scoped", scope=> "sc2"});
+    my $keyname = subname();
 
-    $this->assert_equals("key: remembered_scoped, value: v1 <br />\nkey: remembered_scoped, value: v2 <br />\n", $this->do_dump($core)); 
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $setgetplugin->{UndeclaredStoresBehaviour} = 'create';
+    
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, store => "st1", value => "v1", remember => "1"});
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, store => "st2", value => "v2", remember => "1"});
+    
+    my $actual = $this->do_get($setgetplugin, {_DEFAULT => $keyname, store => "st1"});
+    my $actual2 = $this->do_get($setgetplugin, {_DEFAULT => $keyname, store => "st2"});
+
+    #$this->assert_equals("key: $keyname, value: v1 <br />\nkey: $keyname, value: v2 <br />\n", $this->do_dump($setgetplugin)); 
     $this->assert_equals('v1', $actual);
-    $this->assert_equals('v2', $actual);
+    $this->assert_equals('v2', $actual2);
+    
+    $setgetplugin->_dumpStores();
+}
+
+sub test_undeclared_stores_behaviours {
+    my $this = shift;
+    my $keyname = subname();
+    # if UndeclaredStoresBehaviour => 'create', references to an unknown storeName will create a storeName.dat
+    
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $shouldBeDefault = $setgetplugin->_storeFileForStore('default');
+    $this->assert_matches('.*persistentvars.dat$', $shouldBeDefault);
+    
+    $setgetplugin->{UndeclaredStoresBehaviour} = 'create';
+    my $shouldBeNewStore = $setgetplugin->_storeFileForStore('newstore');
+    #$this->assert_matches('.*newstore.dat$', $shouldBeNewStore);
+
+    # if UndeclaredStoresBehaviour => 'revert', references to unknown stores will use persistantvars.dat
+    $setgetplugin->{UndeclaredStoresBehaviour} = 'revert';
+    my $shouldBeRevertedToDefault = $setgetplugin->_storeFileForStore('notallowednewstore');
+    $this->assert_matches('.*persistentvars.dat$', $shouldBeRevertedToDefault);    
+
 }
 
 =pod
-Tests with a known bad scope and remember=1
+Tests with a known bad store and remember=1
 =cut
 
-sub disabled_test_set_scope_remember_bad_scope_needs_to_be_written {
+sub disabled_test_set_store_remember_bad_store_needs_to_be_written {
     my $this = shift;
+    my $keyname = subname();
     
-    $TWiki::cfg{SetGetPlugin}{Scopes} = 'ScopeA, ScopeB';
+    $TWiki::cfg{SetGetPlugin}{stores} = 'storeA, storeB';
     
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
-    $this->do_set($core, {_DEFAULT => "remembered_scoped", value => "v1", scope => "nonExistantScope", remember => "1"});
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    $this->do_set($setgetplugin, {_DEFAULT => $keyname, value => "v1", store => "nonExistantStore", remember => "1"});
     
-    my $actual = $this->do_get($core, {_DEFAULT => "remembered_scoped", scope=> "sc1"});
+    my $actual = $this->do_get($setgetplugin, {_DEFAULT => $keyname, store=> "st1"});
     # TODO - do we want to log bad accesses?
 
-    $this->assert_equals('', $this->do_dump($core)); 
-    $this->assert_equals('', $actual); # Scope "nonExistantScope" doesn't exist - so no value should be written
+    $this->assert_equals('', $this->do_dump($setgetplugin)); 
+    $this->assert_equals('', $actual); # store "nonExistantStore" doesn't exist - so no value should be written
 }
 
 sub load_persistent_vars {
@@ -286,16 +338,16 @@ sub load_persistent_vars {
 }
 
 
-sub test_load_persistent_vars {
+sub junk_test_load_persistent_vars {
     my $this = shift;
-    my $core = new TWiki::Plugins::SetGetPlugin::Core( $debug );
+    my $setgetplugin = new TWiki::Plugins::SetGetPlugin::Core( $debug );
 
     my $PersistentVars = load_persistent_vars($tempStoreFile_not_namespaced);
     print Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
 
     #my $vars = load_into_storable($tempStoreFile_not_namespaced);
     
-    $this->do_dump($core); # differnt 
+    $this->do_dump($setgetplugin); # differnt 
     
     $PersistentVars = load_persistent_vars($tempStoreFile_namespaced);
     print Data::Dumper->Dump([$PersistentVars], [qw(PersistentVars)]);
@@ -304,28 +356,6 @@ sub test_load_persistent_vars {
 
     #my $vars2 = load_into_storable($tempStoreFile_namespaced);
     
-    $this->do_dump($core);
+    $this->do_dump($setgetplugin);
 }
-
-####### If we need to move to Storable instead of eval. 
-
-use Storable qw(lock_store lock_nstore lock_retrieve);
-
-sub load_into_storable {
-    my ($tempStoreFile) = @_;
-    
-    my $hashref = lock_retrieve($tempStoreFile);    
-    return $hashref;
-}
-
-
-sub convert_persistent_vars_to_storeable {
-
-    my ($persistentVarsStoreFile) = @_;
-    my $storableStoreFile = $persistentVarsStoreFile.".storable";
-    my $vars = load_persistent_vars($persistentVarsStoreFile);
-    lock_store($vars, $storableStoreFile);
-}
-
-
 1;
